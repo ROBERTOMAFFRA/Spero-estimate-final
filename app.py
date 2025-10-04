3bc29b9827198d21d7303dcf9b1882acfecf33fb
ROBERTOMAFFRA <sperorestor@outlook.com>
2025-10-04 14:30:55 -0400

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask import Flask, render_template, request, jsonify
 app = Flask(__name__)
 
 # Carregar planilha
-df = pd.read_excel("data/Modelo app estimate.xlsx", sheet_name="Sheet1")
+df = pd.read_excel("estimate.xlsx", sheet_name="Sheet1")
 
 @app.route("/")
 def index():
